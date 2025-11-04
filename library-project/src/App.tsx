3b076246347d7780899e764cb70100adc4f6cc18
navaShechter <n0556775395@gmail.com>
2025-11-04 20:55:00 +0200

    first commit פתיחת הפרויקט

---
+++ library-project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  welcome to the library
+    </div>
+  );
+}
+
+export default App;
